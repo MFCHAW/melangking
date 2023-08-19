16f60701a70374f385e1702f171952a5920fc60e
Chaw Meng Fei <cengfei78@mail.com>
2023-08-19 17:33:25 +0800

    1

--- init_connection.py
+++ init_connection.py
@@ -2,17 +2,4 @@ import streamlit as st
 import pymssql
 
 def qconnection():
-    return pymssql.connect(server=st.secrets["server"], port=st.secrets["port"], user=st.secrets["username"], password=st.secrets["password"], database=st.secrets["database"])  
-    
-    # return pymssql.connect(
-    #     "server="
-    #     + st.secrets["server"]
-    #     + ";port"
-    #     + st.secrets["port"]
-    #     + ";database="
-    #     + st.secrets["database"]
-    #     + ";user"
-    #     + st.secrets["username"]
-    #     + ";password"
-    #     + st.secrets["password"]
-    # )
+    return pymssql.connect(server=st.secrets["server"], port=st.secrets["port"], user=st.secrets["username"], password=st.secrets["password"], database=st.secrets["database"])
